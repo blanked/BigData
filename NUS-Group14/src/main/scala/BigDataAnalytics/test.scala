bc9866f4ae93dfcfdbac8af2642dae77c38f3c4f
blanked <blanked-out@hotmail.com>
2018-10-16 17:01:25 +0800

    Updated POM. Sample code test.scala working

--- NUS-Group14/src/main/scala/BigDataAnalytics/test.scala
+++ NUS-Group14/src/main/scala/BigDataAnalytics/test.scala
@@ -1,20 +1,22 @@
 package BigDataAnalytics
 
-import ml.sparkling.graph.loaders.csv.GraphFromCsv.LoaderParameters.{Delimiter,Quotation}
 import ml.sparkling.graph.api.loaders.GraphLoading.LoadGraph
 import ml.sparkling.graph.loaders.csv.GraphFromCsv.CSV
 import org.apache.spark.SparkContext
 
 
-// initialize your SparkContext as implicit value so it will be passed automatically to graph loading API
-
-
 object test {
   def main(args : Array[String]) {
-    val sc = newSparkContext()
-    implicit ctx:SparkContext=sc
-  val filePath="file:////media/vboxshared/2018Sem1/BigData/test.csv"
-  val graph=LoadGraph.from(CSV(filePath)).using(Delimiter(";")).using(Quotation("'")).load()  
+    
+    // importing spark context as an implicit function...
+    implicit ctx:SparkContext=>new SparkContext()
+    
+    val directory = "/media/vboxshared/2018Sem1/BigData/"
+    val fileName = "test.csv"
+    val filePath = "file://" + directory + fileName
+    val graph=LoadGraph.from(CSV(filePath)).load()  
+    
+    
   }
   
 }
