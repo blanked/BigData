ea2a8904bacfe4f9840180daae2673e3d66fb013
liyan <liyan920517@gmail.com>
2018-10-20 22:06:46 +0800

    change input file to paramerized

--- NUS-Group14/src/main/scala/BigDataAnalytics/LinkPredictionOperator.scala
+++ NUS-Group14/src/main/scala/BigDataAnalytics/LinkPredictionOperator.scala
@@ -1,7 +1,7 @@
 /**
- * @author Hui Jun
- * Description: Sample file for sparkling graph link prediction
- */
+  * @author Hui Jun
+  *         Description: Sample file for sparkling graph link prediction
+  */
 
 package BigDataAnalytics
 
@@ -9,6 +9,8 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
+import ml.sparkling.graph.operators.algorithms.community.pscan.PSCAN
+import ml.sparkling.graph.api.operators.algorithms.community.CommunityDetection.ComponentID
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.graphx
 import ml.sparkling.graph.api.loaders.GraphLoading.LoadGraph
@@ -33,38 +35,43 @@ import ml.sparkling.graph.operators.partitioning.CommunityBasedPartitioning
 import ml.sparkling.graph.operators.algorithms.community.pscan.PSCAN
 
 
-
 object LinkPredictionOperator {
-  
-  
-  
-  def main(args : Array[String]) {
-    println( "Hello World!" )
+
+
+  def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("LinkPredictionOperator").setMaster("local")
     val spark = SparkSession
       .builder()
+      .config(conf)
       .getOrCreate()
 
-/*    val directory = "/Users/liyan/Documents/cs5344/BigData/data/"
-    val fileName = "small_nodate.csv"*/
-    val fs = "gs://"
-    val directory = "dataproc-753f5751-93fe-4649-89ef-cb7a4c923bc1-asia-southeast1/"
-    val fileName = "flickr-full.csv"
+    val filePath = args(0)
+    val outPath = args(1)
+    val threshold = args(2).toInt
+/*    val fs = "file:///"
+    val directory = "Users/liyan/Downloads/first/"
+    val fileName = "first.csv" */
+/*    val fs = "gs://"
+  //  val directory = "dataproc-753f5751-93fe-4649-89ef-cb7a4c923bc1-asia-southeast1/"
+  //  val fileName = "flickr-full.csv"*/
 
-    val filePath =  fs + directory + fileName
-//    implicit sc:SparkContext => spark.sparkContext
+  //  val filePath = fs + directory + fileName
+    //    implicit sc:SparkContext => spark.sparkContext
     implicit val sc = spark.sparkContext
-    val graph:Graph[String, String] = LoadGraph.from(CSV(filePath)).load()
-    
-    val predictedEdges = BasicLinkPredictor.predictLinks(graph, CommonNeighbours, 10, false)
-//    val predictedEdges: RDD[(VertexId, VertexId)] = graph.predictLinks(edgeMeasure=CommonNeighbours,threshold=10, treatAsUndirected=false)
-    
-    println("Size of RDD: " + predictedEdges.count())
-    predictedEdges.saveAsTextFile(fs + directory + "output")
-    
+    val graph: Graph[String, String] = LoadGraph.from(CSV(filePath)).load()
+
+    val components: Graph[ComponentID, String] = PSCAN.computeConnectedComponents(graph)
+    components.saveAsTextFile(outPath)
+
+    val predictedEdges = BasicLinkPredictor.predictLinks(graph, CommonNeighbours, threshold, false)
+    //    val predictedEdges: RDD[(VertexId, VertexId)] = graph.predictLinks(edgeMeasure=CommonNeighbours,threshold=10, treatAsUndirected=false)
+
+    //println("Size of RDD: " + predictedEdges.count())
+    predictedEdges.saveAsTextFile(outPath)
+
 
     println("Complete!")
-    
+
   }
 
 }
