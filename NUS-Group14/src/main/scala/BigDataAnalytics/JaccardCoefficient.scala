d93170c30b030c170711a29d87c6dabc35a3c170
liyan <liyan920517@gmail.com>
2018-10-21 11:47:05 +0800

    add JaccardCoefficient

---
+++ NUS-Group14/src/main/scala/BigDataAnalytics/JaccardCoefficient.scala
@@ -0,0 +1,30 @@
+package BigDataAnalytics
+
+import ml.sparkling.graph.api.operators.algorithms.shortestpaths.ShortestPathsTypes.{JLong, JSet}
+import ml.sparkling.graph.api.operators.measures.EdgeMeasure
+import ml.sparkling.graph.operators.measures.utils.NeighboursUtils
+import ml.sparkling.graph.operators.measures.utils.NeighboursUtils.NeighbourSet
+import org.apache.spark.graphx.Graph
+import scala.collection.JavaConversions._
+
+import scala.reflect.ClassTag
+
+/**
+ * Created by Roman Bartusiak (roman.bartusiak@pwr.edu.pl http://riomus.github.io).
+ */
+object JaccardCoefficient extends EdgeMeasure[Int,NeighbourSet]{
+
+  def computeValue(srcAttr:NeighbourSet,dstAttr:NeighbourSet,treatAsUndirected:Boolean=false):Int={
+    unionSize(srcAttr,dstAttr)
+  }
+
+
+  def unionSize(neighbours1:JSet[JLong],neighbours2:JSet[JLong]):Int = {
+    neighbours1.union(neighbours2).size
+  }
+
+
+  override def preprocess[VD:ClassTag,E:ClassTag](graph: Graph[VD, E],treatAsUndirected:Boolean=false): Graph[NeighbourSet, E] = {
+    NeighboursUtils.getWithNeighbours(graph,treatAsUndirected)
+  }
+}
